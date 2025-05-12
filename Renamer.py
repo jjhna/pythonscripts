3212995e49e4ca150fe6adc0bf97d361a5fb97a1
Jon <22411356+jjhna@users.noreply.github.com>
2025-05-12 07:58:04 -0500

    Update Renamer with ability to edit the prefix name for the files
    Update Renamer with ability to edit the prefix name for the files
    
    and some additional notes on the f-string explanation

--- Renamer.py
+++ Renamer.py
@@ -11,19 +11,28 @@ import os
 # note for a nested structure you would use something like: root = os.path.join('..', 'folder1', 'folder2', 'folder3')
 root = os.path.join('..', 'test_folder')
 
+# Nice, how about a script that requests the new prefix for the file names
+newinput = input("Type in your new prefix for the file names: ")
+
+# the walk function will traverse the directory trees and visits all directories, folders and files
+# in this case it visits all the files from the root directory where this script is being called from 
 for directory, subdir_list, file_list in os.walk(root):
     for name in file_list:
         # maps the path of the current directory with the name of the file in the entire file list
         source_name = os.path.join(directory, name)
         #timestamp = os.path.getmtime(source_name)
         #modified_date = str(datetime.datetime.fromtimestamp(timestamp)).replace(':','.')
+
+        # remember that the f character below stands for an f-string or formatted string literal
+        # it allows yo uto embed expressions inside the string literals from other variables inside the curly braces
         
         # joins the the os path with the word testing_ prefix in front of it
-        target_name = os.path.join(directory, f'testing_{name}')
+        target_name = os.path.join(directory, f'{newinput}_{name}')
 
         print(f"Directory: {directory}")
         print(f"Subdirectories: {subdir_list}")
         print(f"Files: {file_list}")
+        print(f"Input: {newinput}")
         print(f'Renaming: {source_name} to: {target_name}')
 
         # built in os function rename which will rename the source_name to the target_name
