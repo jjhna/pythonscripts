e5dbd0db212ab5d14e577fbf1b3daa1543edd856
Jon <22411356+jjhna@users.noreply.github.com>
2025-07-06 10:12:59 -0500

    Python create insert statements script from csv files - in progress - part 2
    Python create insert statements script from csv files - in progress - part 2
    
    Finalized the insert statement with Arrays and JSON entires

--- csv2insertTemplate.py
+++ csv2insertTemplate.py
@@ -7,18 +7,41 @@ import json
 
 # print out the select query with each cast column with the as statement
 def columnsplit(filename2, tableName2): 
-    total = "INSERT INTO " + tableName2 + " ("
+    # total variable is what we will print out back to the user
+    total = "\nINSERT INTO " + tableName2 + " ("
     with open(filename2, 'r', newline='') as csvfile:
         csvreader = csv.DictReader(csvfile, delimiter='\t')
-        column3Split = ""
-        for row in csvreader:
-            column3Split = row['Speicalpowa']
-        column3SingleLine = json.loads(column3Split)
-        for j in column3SingleLine:
-            #print(f"Key: {item['Key']}, Value: {item['VALUE']}")
-            for key, value in j.items():
-                print(f"Key: {key}, Value: {value}")
 
+        # Iterates through each of the rows in the csv file
+        for row in csvreader:
+            rowKeys = row # Takes the value of the first row
+            column0Split = row['Name'] # the contents of the columns under xxx
+            column1Split = row['Age']
+            column2Split = row['Sex']
+            column3Split = row['Speicalpowa']
+        
+        # Takes the values of the first row and adds it to the total variable 
+        for i in rowKeys:
+            total += "'" + i + "', "
+        total += ")\n" + "VALUES ("
+
+        # Then we get the contents from the first 3 columns and add it to the total 
+        total += "'" + column0Split + "','" + column1Split + "','" + column2Split + "',[\n"
+
+        # For any columns that contain a JSON like variable will  be iterated through as a list and then inner looped through as a key value pair
+        column3SingleLine = json.loads(column3Split) 
+        for j in column3SingleLine:
+            # print(f"Key: {item['Key']}, Value: {item['VALUE']}")
+            for key, value in j.items():
+                #print(f"Key: {key}, Value: {value}")
+                if key == "Key":
+                    total += "ARRAY('" + value
+                else:
+                    total += "','" + value + "'),\n"
+        total += "],\n"
+        total += "function1()\n"
+        total += "function2());\n"
+        print(total)
 
 """
         strippy = firstRow[0].split('\t')
@@ -31,13 +54,11 @@ def columnsplit(filename2, tableName2):
         total += strippy2[1] + ", "
         total += strippy2[2] + ", "
         #total += strippy2[3]
-        total += ""
-        print(total)
 """
 
 # get the csv file from the user, please note that the user only needs to type in the file name in the same directory and doesn't need to add in .csv suffix 
-#filename = input("Please enter the csv file name you want to convert to select SQL query: ")
-#filename = filename + ".csv"
-#tableName = input("Please enter in both the DB and table name you want to insert into:   ")
-#columnsplit(filename, tableName)
-columnsplit("book1.csv", "aba")
+filename = input("Please enter the csv file name you want to convert to select SQL query: ")
+filename = filename + ".csv"
+tableName = input("Please enter in both the DB and table name you want to insert into:   ")
+columnsplit(filename, tableName)
+#columnsplit("book1.csv", "aba")
