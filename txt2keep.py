c221f3110cc5047a1d03062f8c7b8c60ad3cbae7
Jon <22411356+jjhna@users.noreply.github.com>
2025-07-15 16:02:28 -0500

    Python created a script to read text files and edit out any keywords in the text file
    Python created a script to read text files and edit out any keywords in the text file

---
+++ txt2keep.py
@@ -0,0 +1,30 @@
+# Read a txt file and only keep certain rows that contain the certain keywords
+# cd D:\PythonProject\pythonscripts
+# python .\txt2keep.py
+
+def checkstring(somestring):
+    match somestring.upper().strip(",\n"):
+        case "DB":
+            return "DB."
+        case "INT":
+            return "int"
+
+# print out the select query with each cast column without the as statement
+def copytextfile(filename2): 
+    total = ""
+    with open(filename2, 'r') as txtfile:
+        for row in txtfile:
+            rowsplit = row.split(' ')
+            listofstuff = ["INT","STRING"] # list of strings you want to edit out, make sure its captialized 
+            for s in rowsplit:
+                if any(i in s.upper() for i in listofstuff): # any() will return true if any of the iterable items are true, so the i is = to the s string and checks through the list for any related items
+                    total += "sum " + s + " "
+                else:
+                    total += s + " "
+        print(total)
+
+# get the csv file from the user, please note that the user only needs to type in the file name in the same directory and doesn't need to add in .csv suffix 
+filename = input("Please enter the txt file name you want to convert to select SQL query: ")
+filename = filename + ".txt"
+print(filename)
+copytextfile(filename)
