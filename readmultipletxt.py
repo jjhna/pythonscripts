45695f26b039db0981e344eba0e957756585cf88
Jon <22411356+jjhna@users.noreply.github.com>
2025-07-22 08:33:05 -0500

    Python replace keywords when coming across them in the text file
    Python replace keywords when coming across them in the text file

--- readmultipletxt.py
+++ readmultipletxt.py
@@ -3,18 +3,6 @@
 # cd D:\PythonProject\pythonscripts
 # python .\readmultipletxt.py
 
-# function to return only the specific names of the files from the list of files
-def getListofFileNames(totalFiles):
-    totalList = []
-    invalidwords = ["<", "/>"]
-    totalFiles = totalFiles.split('"')
-    for row in totalFiles:
-       if all(e not in row for e in invalidwords):
-           totalList.append(row)
-    return totalList
-
-    # if not < /> or \n then continue
-
 # function to return the list of files from the original text files
 def getListofFiles(filename, filename2): 
     total = ""
@@ -33,6 +21,24 @@ def getListofFiles(filename, filename2):
                     total += s
     return total
 
+# function to return only the specific names of the files from the list of files
+def getListofFileNames(totalFiles):
+    totalList = []
+    invalidwords = ["<", "/>"]
+    totalFiles = totalFiles.split('"')
+    for row in totalFiles:
+       if all(e not in row for e in invalidwords):
+           row = row.replace("/", "\\")
+           totalList.append(row)
+    return totalList
+
+# function to open the list of files to get the table names
+def openfileList(foldername, filelist):
+    for i in filelist:
+        filenamefull = "D:\\PythonProject\\SAMPLE_SERVER\\" + foldername + "\\" + i
+        print(filenamefull)
+
+
 def main():
     # get the csv file from the user, please note that the user only needs to type in the file name in the same directory and doesn't need to add in .csv suffix 
     fileName = ""
@@ -41,13 +47,13 @@ def main():
     filenametemp = "D:\\PythonProject\\SAMPLE_SERVER\\" + filename
     print(filenametemp)
     filename2 = input("Please enter the the Log file name you want to utilize: ")
-    filename2 = filename2 + ".txt"
+    filename2 = filename2 + ".xml"
     filenametemp = filenametemp + "\\LogFiles\\" + filename2
     print(filenametemp)
 
     fileName = getListofFiles(filename, filename2)
     fileNameList = getListofFileNames(fileName)
-    print(fileNameList)
+    openfileList(filename, fileNameList)
 
 
 if __name__ == "__main__":
