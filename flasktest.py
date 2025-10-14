6cc1ca81fbfe84081cd718c811c3b3fcf4365771
Jon <22411356+jjhna@users.noreply.github.com>
2025-10-14 08:33:05 -0500

    Flask and JS and HTML
    Flask JS and HTML
    Made a checkbox that activates or deactivates the textbox

--- flasktest.py
+++ flasktest.py
@@ -2,9 +2,21 @@
 # cd D:\PythonProject\pythonscripts
 # python .\flasktest.py
 
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
+@app.route('/frontpage', methods=['GET','POST'])
+def frontpage():
+    if request.method == 'POST':
+        textinputbox = request.form['textbox2']
+        checkboxbool = 'checkbox1' in request.form
+        textbox1result = request.form.get('textbox1')
+        if checkboxbool:
+            textbox1result += " is the magic word"
+        else:
+            textbox1result = "Default"
+    return render_template('flasktest.html', resulttextbox1 = textbox1result, resulttextbox2 = textinputbox) 
 
 @app.route('/')
 def home():
