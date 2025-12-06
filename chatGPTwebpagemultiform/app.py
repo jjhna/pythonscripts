0ef9e29f4be91622cf0be8661e16d1658e190853
Jon <22411356+jjhna@users.noreply.github.com>
2025-12-06 16:40:57 -0600

    Add 3 pages
    Add 3 pages and navbar

--- chatGPTwebpagemultiform/app.py
+++ chatGPTwebpagemultiform/app.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def home():
     return send_from_directory('home', 'home.html')
 
+@app.route('/form1')
+def form1():
+    return send_from_directory('form1', 'form1.html')
+
+@app.route('/settings')
+def settings():
+    return send_from_directory('settings', 'settings.html')
+
+
+@app.route('/navbar/<path:filename>')
+def navbar_files(filename):
+    # Serve the reusable navbar HTML (and any files placed in navbar/)
+    return send_from_directory('navbar', filename)
+
 if __name__ == '__main__':
     app.run(debug=True)
